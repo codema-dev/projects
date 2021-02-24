2b6b532e0cb41f66f101e5c459bf8725c0c577ed
Rowan Molony <rowan.molony@codema.ie>
2021-02-24 17:11:21 +0000

    Get total annual stock numbers (#4)
    Can link these totals to the GNI Postcode gas
    demand data averages to get rough heat demand estimates

---
+++ get_stock_annual_totals.py
@@ -0,0 +1,60 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: 'Python 3.9.2 64-bit (''dbs'': conda)'
+#     metadata:
+#       interpreter:
+#         hash: 91b4ddbfb3ef6e4cea71efeb7c8e9fed5f228fde8c807fc357f7d211c22b6ea4
+#     name: python3
+# ---
+
+# %%
+from os import path
+from shutil import unpack_archive
+from urllib.request import urlretrieve
+
+import pandas as pd
+
+# %%
+path_to_census_stock = "data/dublin_building_stock_up_to_2011.csv"
+if not path.exists(path_to_census_stock):
+    urlretrieve(
+        url="https://zenodo.org/record/4552498/files/dublin_building_stock_up_to_2011.csv",
+        filename=path_to_census_stock,
+    )
+residential_stock_pre_2011 = pd.read_csv(path_to_census_stock)
+
+# %%
+total_pre_2011 = (
+    residential_stock_pre_2011.groupby("postcodes")["dwelling_type_unstandardised"]
+    .count()
+    .rename("pre_2011")
+)
+
+# %%
+berpublicsearch_ireland = dd.read_parquet("data/BERPublicsearch_parquet")
+berpublicsearch_dublin = (
+    berpublicsearch_ireland[
+        berpublicsearch_ireland["CountyName"].str.contains("Dublin")
+    ]
+    .query("`CountyName` != ['Dublin 23']")  # doesn't exist
+    .compute()
+)
+
+# %%
+for year in range(2016, 2021, 1):
+    total = total_pre_2011 + (
+        berpublicsearch_dublin.query(
+            f"`Year_of_Construction` > 2011 and `Year_of_Construction` <= {year}"
+        )
+        .groupby("CountyName")["DwellingTypeDescr"]
+        .count()
+        .rename(f"post_{year}")
+    )
+    total.to_csv(f"data/residential_total_buildings_{year}.csv")
