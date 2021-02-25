75a4795f692eaba5402cfd253d96d6ccf2ac9ef5
Rowan Molony <rowan.molony@codema.ie>
2021-02-25 11:30:28 +0000

    Save stock totals in same format as annual gas
    With a column of postcode totals per year

--- notebooks/get_stock_annual_totals.py
+++ notebooks/get_stock_annual_totals.py
@@ -19,8 +19,10 @@ from os import path
 from shutil import unpack_archive
 from urllib.request import urlretrieve
 
+import dask.dataframe as dd
 from berpublicsearch.download import download_berpublicsearch_parquet
 import pandas as pd
+import numpy as np
 
 # %%
 path_to_census_stock = "../data/dublin_building_stock_up_to_2011.csv"
@@ -30,13 +32,6 @@ if not path.exists(path_to_census_stock):
         filename=path_to_census_stock,
     )
 residential_stock_pre_2011 = pd.read_csv(path_to_census_stock)
-
-# %%
-total_pre_2011 = (
-    residential_stock_pre_2011.groupby("postcodes")["dwelling_type_unstandardised"]
-    .count()
-    .rename("pre_2011")
-)
 
 # %%
 path_to_berpublicsearch = "../data/BERPublicsearch_parquet"
@@ -55,13 +50,27 @@ berpublicsearch_dublin = (
 )
 
 # %%
-for year in range(2016, 2021, 1):
-    total = total_pre_2011 + (
+total_pre_2010 = residential_stock_pre_2011.groupby("postcodes")[
+    "dwelling_type_unstandardised"
+].count()
+
+# %%
+totals = []
+for year in range(2011, 2020, 1):
+    total_from_2010_to_year = (
         berpublicsearch_dublin.query(
-            f"`Year_of_Construction` > 2011 and `Year_of_Construction` <= {year}"
+            f"`Year_of_Construction` > 2010 and `Year_of_Construction` <= {year}"
         )
         .groupby("CountyName")["DwellingTypeDescr"]
         .count()
-        .rename(f"post_{year}")
+        .reindex(total_pre_2010.index)
+        .fillna(0)
     )
-    total.to_csv(f"../data/residential_total_buildings_{year}.csv")
+    total = total_pre_2010 + total_from_2010_to_year
+    total = total.rename(year)
+    totals.append(total)
+
+total_by_year = pd.concat(totals, axis="columns")
+
+# %%
+total_by_year.to_csv(f"../data/residential_total_buildings_by_year.csv")
