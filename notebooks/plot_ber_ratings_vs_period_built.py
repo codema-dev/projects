1bd684a6366ea1bb2ffb279635c01245be074fd7
Rowan Molony <rowan.molony@codema.ie>
2021-03-29 16:31:00 +0100

    Plot BER Rating vs Building Period (#27)
    Distribution can be used to guess BER ratings of
    any building based solely on age

--- notebooks/plot_ber_ratings_vs_period_built.py
+++ notebooks/plot_ber_ratings_vs_period_built.py
@@ -1,17 +1,14 @@
 # %%
 from pathlib import Path
 
-import dask.dataframe as dd
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
-sns.set()
+import pandas_bokeh
+
+# pandas_bokeh.output_notebook()
 data_dir = Path("../data")
-
-# %%
-ber_public = dd.read_parquet(data_dir / "BERPublicsearch_parquet")
+html_dir = Path("../html")
 
 # %%
 keep_columns = [
@@ -19,39 +16,36 @@ keep_columns = [
     "Year_of_Construction",
     "EnergyRating",
 ]
-ber_dublin = (
-    ber_public.loc[:, keep_columns]
-    .loc[ber_public["CountyName"].str.contains("Dublin")]
-    .compute()
-    .assign(
-        cso_period_built=lambda df: pd.cut(
-            df["Year_of_Construction"],
-            bins=[-np.inf, 1919, 1945, 1960, 1970, 1980, 1990, 2000, 2010, np.inf],
-            labels=[
-                "before 1919",
-                "1919 - 1945",
-                "1946 - 1960",
-                "1961 - 1970",
-                "1971 - 1980",
-                "1981 - 1990",
-                "1991 - 2000",
-                "2001 - 2010",
-                "2011 or later",
-            ],
-        ),
-        cso_period_built_count=lambda df: df.groupby("EnergyRating")["cso_period_built"]
-        .value_counts()
-        .reset_index(drop=True),
-        age_bin=lambda df: pd.cut(
-            df["Year_of_Construction"],
-            bins=[-np.inf, 2000, 2010, np.inf],
-            labels=[
-                "before 2000",
-                "2001 - 2010",
-                "2011 or later",
-            ],
-        ),
-    )
+ber_dublin = pd.read_parquet(data_dir / "BERPublicsearch_Dublin.parquet").assign(
+    cso_period_built=lambda df: pd.cut(
+        df["Year_of_Construction"],
+        bins=[-np.inf, 1919, 1945, 1960, 1970, 1980, 1990, 2000, 2010, np.inf],
+        labels=[
+            "before 1919",
+            "1919 - 1945",
+            "1946 - 1960",
+            "1961 - 1970",
+            "1971 - 1980",
+            "1981 - 1990",
+            "1991 - 2000",
+            "2001 - 2010",
+            "2011 or later",
+        ],
+    ),
+    BerBand=lambda df: df["EnergyRating"].str[0],
+    EstimatedBer=lambda df: df["cso_period_built"].replace(
+        {
+            "before 1919": "E",
+            "1919 - 1945": "E",
+            "1946 - 1960": "E",
+            "1961 - 1970": "D",
+            "1971 - 1980": "D",
+            "1981 - 1990": "D",
+            "1991 - 2000": "D",
+            "2001 - 2010": "C",
+            "2011 or later": "A",
+        }
+    ),
 )
 
 # %%
@@ -60,14 +54,20 @@ ber_dublin.pivot_table(
     columns="cso_period_built",
     values="Year_of_Construction",
     aggfunc="count",
-).plot.bar(rot=0, stacked=True, figsize=(15, 5))
+).plot_bokeh.bar()
+
+# %%
+pandas_bokeh.output_file(html_dir / "plot_ber_ratings_vs_period_built.html")
 
 # %%
 ber_dublin.pivot_table(
-    index="EnergyRating",
-    columns="age_bin",
-    values="age_bin_count",
-    aggfunc="sum",
-).plot.bar(rot=0, stacked=True, figsize=(15, 5))
+    index="BerBand",
+    columns="cso_period_built",
+    values="Year_of_Construction",
+    aggfunc="count",
+).plot_bokeh.bar(ylabel="Number of Buildings")
+
+# %%
+pandas_bokeh.output_file(html_dir / "plot_ber_bands_vs_period_built.html")
 
 # %%
