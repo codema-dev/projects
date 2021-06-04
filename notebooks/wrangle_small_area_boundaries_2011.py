ec8be2ab4454607b8301a57e5e3c18bfb0c6bc2b
oisindoherty3 <69256705+oisindoherty3@users.noreply.github.com>
2021-06-04 12:27:45 +0100

    add distance from sa centroid to city centre (#85)

--- notebooks/wrangle_small_area_boundaries_2011.py
+++ notebooks/wrangle_small_area_boundaries_2011.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from shapely.geometry import Point
 import geopandas as gpd
 
 from dublin_building_stock.spatial_operations import get_geometries_within
@@ -24,11 +25,19 @@ ireland_small_area_boundaries = gpd.read_file(
     data_dir / "Census2011_Small_Areas_generalised20m"
 )[use_columns]
 
+temple_bar_location = Point(715643, 734177)
+m_to_km = 1 / 1000
 dublin_small_area_boundaries = (
     ireland_small_area_boundaries.to_crs(epsg=2157)
     .pipe(get_geometries_within, dublin_boundary.to_crs(epsg=2157))
     .pipe(get_geometries_within, dublin_routing_key_boundaries.to_crs(epsg=2157))
     .pipe(get_geometries_within, dublin_local_authority_boundaries.to_crs(epsg=2157))
+    .assign(
+        distance_to_city_centre_in_km=lambda gdf: gdf.geometry.representative_point()
+        .distance(temple_bar_location)
+        .multiply(m_to_km)
+        .round(2)
+    )
 )
 
 dublin_small_area_boundaries.to_file(
