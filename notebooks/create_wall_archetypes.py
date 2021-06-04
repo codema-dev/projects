4f0bab5a24ef7012b0f7ef5ec6c853c44e806dda
oisindoherty3 <69256705+oisindoherty3@users.noreply.github.com>
2021-06-04 15:38:46 +0100

    Add walls location (#87)
    * add distance from sa centroid to city centre
    
    * Create wall archertypes and generate modal wall type
    
    * Break modal split by banded proximity to cc

--- notebooks/create_wall_archetypes.py
+++ notebooks/create_wall_archetypes.py
@@ -10,11 +10,15 @@ dwelling_type = "dwelling_type"
 period_built = "period_built"
 wall = "FirstWallType_Description"
 rename = "ModeWallDescription"
+location = "location"
+cc_rename = "LocationModeWallDescription"
+
+df[location] = df[distance_to_cc].apply(lambda x: 'city_centre' if x <= 3.5 else 'outside')
 
 mode_wall_types = (
-    df.groupby([distance_to_cc, dwelling_type, period_built])[wall]
+    df.groupby([location, dwelling_type, period_built])[wall]
     .agg(pd.Series.mode)
     .rename(rename)
 )
 
-mode_wall_types.to_csv(data_dir / "mode_wall_bands.csv")
+mode_wall_types.to_csv(data_dir / "mode_wall_bands_postcode.csv")
