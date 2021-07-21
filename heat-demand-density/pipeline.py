b79eba04c5ea9f1a32bb6ac1e538d71cda45da40
Rowan Molony <rowan.molony@codema.ie>
2021-07-21 18:24:53 +0100

    Split pipeline, tasks & functions
    Use global data_dir and config to define tasks & pipeline instead of
    passing them as arguments to a pipeline function as this enables
    moving task definitions to a separate module leaving only pipeline
    code.  DATA_DIR and CONFIG can be mocked out by monkeypatching
    the functions generating these globals prior to importing the pipeline
    run code - i.e. must be imported from within the test function

--- heat-demand-density/pipeline.py
+++ heat-demand-density/pipeline.py
@@ -1,191 +1,66 @@
-from configparser import ConfigParser
-import os
-from pathlib import Path
-from typing import Any
-from typing import Dict
-
 import dotenv
-from fsspec.registry import filesystem
 
-dotenv.load_dotenv(".prefect")  # local local prefect configuration
+dotenv.load_dotenv(".prefect")  # load local prefect configuration prior to import!
 import prefect
-from prefect.engine import results
-from prefect.engine import serializers
 
+from globals import CONFIG
+from globals import DATA_DIR
 import tasks
-from serializers import GeoPandasSerializer
 
 
-HERE = Path(__name__).parent
-CONFIG = ConfigParser()
-CONFIG.read(HERE / "config.ini")
-DATA_DIR = HERE / "data"
 dotenv.load_dotenv()  # load s3 credentials
 
+with prefect.Flow("Estimate Heat Demand Density") as flow:
+    # Set CONFIG
+    assumed_boiler_efficiency = prefect.Parameter(
+        "Assumed Boiler Efficiency", default=0.9
+    )
 
-def get_json_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=serializers.JSONSerializer(),
+    # Extract
+    valuation_office = tasks.load_valuation_office(
+        url=CONFIG["valuation_office"]["url"]
+    )
+    bers = tasks.load_bers(url=CONFIG["bers"]["url"])
+    benchmark_uses = tasks.load_benchmark_uses(
+        url=CONFIG["benchmark_uses"]["url"], filesystem_name="s3"
+    )
+    benchmarks = tasks.load_benchmarks(url=CONFIG["benchmarks"]["url"])
+    small_area_boundaries = tasks.load_small_area_boundaries(
+        url=CONFIG["small_area_boundaries"]["url"]
+    )
+    local_authority_boundaries = tasks.load_local_authority_boundaries(
+        url=CONFIG["local_authority_boundaries"]["url"]
+    )
+
+    # Estimate Demand
+    valuation_office_map = tasks.link_valuation_office_to_small_areas(
+        valuation_office=valuation_office,
+        small_area_boundaries=small_area_boundaries,
+    )
+    non_residential_demand = tasks.apply_benchmarks_to_valuation_office_floor_areas(
+        valuation_office=valuation_office_map,
+        benchmark_uses=benchmark_uses,
+        benchmarks=benchmarks,
+        assumed_boiler_efficiency=assumed_boiler_efficiency,
+    )
+    residential_demand = tasks.extract_residential_heat_demand(bers)
+    demand_mwh_per_y = tasks.amalgamate_heat_demands_to_small_areas(
+        residential=residential_demand, non_residential=non_residential_demand
+    )
+    demand_tj_per_km2 = tasks.convert_from_mwh_per_y_to_tj_per_km2(
+        demand=demand_mwh_per_y, small_area_boundaries=small_area_boundaries
+    )
+
+    # Map Demand
+    boundaries = tasks.link_small_areas_to_local_authorities(
+        small_area_boundaries=small_area_boundaries,
+        local_authority_boundaries=local_authority_boundaries,
+    )
+    tasks.map_demand(
+        demand=demand_tj_per_km2,
+        boundaries=boundaries,
+        filepath=DATA_DIR / "processed" / "dublin_small_area_demand_tj_per_km2.geojson",
     )
 
 
-def get_parquet_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=serializers.PandasSerializer("parquet"),
-    )
-
-
-def get_geoparquet_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=GeoPandasSerializer("parquet"),
-    )
-
-
-def estimate_heat_demand_density(
-    config: ConfigParser = CONFIG, data_dir: Path = DATA_DIR
-):
-    load_valuation_office = prefect.task(
-        tasks.read_parquet,
-        target="raw_valuation_office.parquet",
-        checkpoint=True,
-        result=get_parquet_result(data_dir / "external"),
-        name="Load Dublin Valuation Office",
-    )
-    load_bers = prefect.task(
-        tasks.read_parquet,
-        target="small_area_bers.parquet",
-        checkpoint=True,
-        result=get_parquet_result(data_dir / "external"),
-        name="Load Dublin Small Area BERs",
-    )
-    load_benchmark_uses = prefect.task(
-        tasks.read_benchmark_uses,
-        target="benchmark_uses.json",
-        checkpoint=True,
-        result=get_json_result(data_dir / "external"),
-        name="Load Valuation Office Benchmark Uses",
-    )
-    load_benchmarks = prefect.task(
-        tasks.read_excel,
-        target="benchmarks.parquet",
-        checkpoint=True,
-        result=get_parquet_result(data_dir / "external"),
-        name="Load Energy Benchmarks",
-    )
-    load_small_area_boundaries = prefect.task(
-        tasks.read_geoparquet,
-        target="small_area_boundaries.parquet",
-        checkpoint=True,
-        result=get_geoparquet_result(data_dir / "external"),
-        name="Load Dublin Small Area Boundaries",
-    )
-    load_local_authority_boundaries = prefect.task(
-        tasks.read_zipped_shp,
-        target="local_authority_boundaries.parquet",
-        checkpoint=True,
-        result=get_geoparquet_result(data_dir / "external"),
-        name="Load Dublin Local Authority Boundaries",
-    )
-
-    link_small_areas_to_local_authorities = prefect.task(
-        tasks.link_small_areas_to_local_authorities,
-        name="Link Each Small Areas to their Corresponding Local Authority",
-    )
-    apply_benchmarks_to_valuation_office_floor_areas = prefect.task(
-        tasks.apply_benchmarks_to_valuation_office_floor_areas,
-        name="Apply Energy Benchmarks to Valuation Office Floor Areas",
-    )
-    link_valuation_office_to_small_areas = prefect.task(
-        tasks.link_valuation_office_to_small_areas,
-        target="valuation_office_small_areas.parquet",
-        checkpoint=True,
-        result=get_geoparquet_result(data_dir / "interim"),
-        name="Link Valuation Office to Small Area Boundaries",
-    )
-    extract_residential_heat_demand = prefect.task(
-        tasks.extract_residential_heat_demand,
-        name="Extract DEAP Residential Heat Demand",
-    )
-    amalgamate_heat_demands_to_small_areas = prefect.task(
-        tasks.amalgamate_heat_demands_to_small_areas,
-        target="dublin_small_area_demand_mwh_per_y.parquet",
-        checkpoint=True,
-        result=get_parquet_result(data_dir / "processed"),
-        name="Amalgamate Heat Demands to Small Areas",
-    )
-    convert_from_mwh_per_y_to_tj_per_km2 = prefect.task(
-        tasks.convert_from_mwh_per_y_to_tj_per_km2,
-        target="dublin_small_area_demand_tj_per_km2.parquet",
-        checkpoint=True,
-        result=get_parquet_result(data_dir / "processed"),
-        name="Convert Heat Demands from MWh/year to TJ/km2",
-    )
-    map_demand = prefect.task(
-        tasks.map_demand,
-        name="Map Small Area Heat Demands",
-    )
-
-    with prefect.Flow("Estimate Heat Demand Density") as flow:
-        # Set Config
-        assumed_boiler_efficiency = prefect.Parameter(
-            "Assumed Boiler Efficiency", default=0.9
-        )
-
-        # Extract
-        valuation_office = load_valuation_office(url=config["valuation_office"]["url"])
-        bers = load_bers(url=config["bers"]["url"])
-        benchmark_uses = load_benchmark_uses(
-            url=config["benchmark_uses"]["url"], filesystem_name="s3"
-        )
-        benchmarks = load_benchmarks(url=config["benchmarks"]["url"])
-        small_area_boundaries = load_small_area_boundaries(
-            url=config["small_area_boundaries"]["url"]
-        )
-        local_authority_boundaries = load_local_authority_boundaries(
-            url=config["local_authority_boundaries"]["url"]
-        )
-
-        # Estimate Demand
-        valuation_office_map = link_valuation_office_to_small_areas(
-            valuation_office=valuation_office,
-            small_area_boundaries=small_area_boundaries,
-        )
-        non_residential_demand = apply_benchmarks_to_valuation_office_floor_areas(
-            valuation_office=valuation_office_map,
-            benchmark_uses=benchmark_uses,
-            benchmarks=benchmarks,
-            assumed_boiler_efficiency=assumed_boiler_efficiency,
-        )
-        residential_demand = extract_residential_heat_demand(bers)
-        demand_mwh_per_y = amalgamate_heat_demands_to_small_areas(
-            residential=residential_demand, non_residential=non_residential_demand
-        )
-        demand_tj_per_km2 = convert_from_mwh_per_y_to_tj_per_km2(
-            demand=demand_mwh_per_y, small_area_boundaries=small_area_boundaries
-        )
-
-        # Map Demand
-        boundaries = link_small_areas_to_local_authorities(
-            small_area_boundaries=small_area_boundaries,
-            local_authority_boundaries=local_authority_boundaries,
-        )
-        map_demand(
-            demand=demand_tj_per_km2,
-            boundaries=boundaries,
-            filepath=data_dir
-            / "processed"
-            / "dublin_small_area_demand_tj_per_km2.geojson",
-        )
-
-    ## Run flow!
-    from prefect.utilities.debug import raise_on_exception
-
-    with raise_on_exception():
-        flow.run()
-
-
-if __name__ == "__main__":
-    estimate_heat_demand_density()
+flow.run()
--- heat-demand-density/tasks.py
+++ heat-demand-density/tasks.py
@@ -1,161 +1,123 @@
-from collections import defaultdict
 from pathlib import Path
 
-import fsspec
-import geopandas as gpd
-import pandas as pd
+import prefect
+from prefect.engine import results
+from prefect.engine import serializers
+
+import functions
+from globals import DATA_DIR
+from serializers import GeoPandasSerializer
 
 
-def read_excel(url: str) -> pd.DataFrame:
-    with fsspec.open(url) as f:
-        return pd.read_excel(f)
-
-
-def read_parquet(url: str) -> pd.DataFrame:
-    with fsspec.open(url) as f:
-        return pd.read_parquet(f)
-
-
-def read_geoparquet(url: str) -> pd.DataFrame:
-    with fsspec.open(url) as f:
-        return gpd.read_parquet(f)
-
-
-def read_zipped_shp(url: str) -> pd.DataFrame:
-    with fsspec.open(url) as f:
-        return gpd.read_file(f)
-
-
-def read_benchmark_uses(url: str, filesystem_name: str) -> pd.DataFrame:
-    fs = fsspec.filesystem(filesystem_name)
-    uses_grouped_by_category = defaultdict()
-    for file in fs.glob(url + "/*.txt"):
-        name = file.split("/")[-1].replace(".txt", "")
-        with fs.open(file, "r") as f:
-            uses_grouped_by_category[name] = [line.rstrip() for line in f]
-    return {i: k for k, v in uses_grouped_by_category.items() for i in v}
-
-
-def link_valuation_office_to_small_areas(
-    valuation_office: pd.DataFrame, small_area_boundaries: gpd.GeoDataFrame
-) -> pd.DataFrame:
-    valuation_office_map = gpd.GeoDataFrame(
-        valuation_office,
-        geometry=gpd.points_from_xy(
-            x=valuation_office["X_ITM"], y=valuation_office["Y_ITM"], crs="EPSG:2157"
-        ),
-    )
-    return gpd.sjoin(
-        valuation_office_map, small_area_boundaries.to_crs(epsg=2157), op="within"
-    ).drop(columns="index_right")
-
-
-def link_small_areas_to_local_authorities(
-    small_area_boundaries: gpd.GeoDataFrame,
-    local_authority_boundaries: gpd.GeoDataFrame,
-) -> gpd.GeoDataFrame:
-    small_area_points = small_area_boundaries.copy()
-    small_area_points[
-        "geometry"
-    ] = small_area_boundaries.geometry.representative_point()
-
-    # NOTE: fiona infers an incorrect encoding for the local authority boundaries
-    small_areas_in_local_authorities = (
-        gpd.sjoin(
-            small_area_points,
-            local_authority_boundaries[["COUNTYNAME", "geometry"]],
-            op="within",
-        )
-        .drop(columns="index_right")
-        .rename(columns={"COUNTYNAME": "local_authority"})
-        .assign(local_authority=lambda df: df["local_authority"].str.decode("latin-1"))
-    )
-    return small_area_boundaries.merge(
-        small_areas_in_local_authorities.drop(columns="geometry")
+def get_json_result(data_dir: Path) -> results.LocalResult:
+    return results.LocalResult(
+        dir=data_dir,
+        serializer=serializers.JSONSerializer(),
     )
 
 
-def apply_benchmarks_to_valuation_office_floor_areas(
-    valuation_office: pd.DataFrame,
-    benchmark_uses: pd.DataFrame,
-    benchmarks: pd.DataFrame,
-    assumed_boiler_efficiency: float,
-) -> pd.Series:
-    valuation_office["use"] = valuation_office["Use1"].map(benchmark_uses)
-    with_benchmarks = valuation_office.merge(
-        benchmarks, left_on="use", right_on="Benchmark", how="left", indicator=True
-    )
-    non_industrial_heat_demand_kwh_per_y = (
-        with_benchmarks["Typical fossil fuel [kWh/m²y]"] * with_benchmarks["Total_SQM"]
-    ) / assumed_boiler_efficiency
-    industrial_heat_demand_kwh_per_y = (
-        with_benchmarks["Industrial space heat [kWh/m²y]"]
-        * with_benchmarks["Total_SQM"]
-    )
-    kwh_to_mwh = 1e-3
-    with_benchmarks["heat_demand_mwh_per_y"] = (
-        non_industrial_heat_demand_kwh_per_y.fillna(0)
-        + industrial_heat_demand_kwh_per_y.fillna(0)
-    ) * kwh_to_mwh
-    return with_benchmarks[["small_area", "heat_demand_mwh_per_y"]]
-
-
-def extract_residential_heat_demand(bers: pd.DataFrame) -> pd.Series:
-    kwh_to_mwh = 1e-3
-    bers["heat_demand_mwh_per_y"] = (
-        bers["main_sh_demand"]
-        + bers["suppl_sh_demand"]
-        + bers["main_hw_demand"]
-        + bers["suppl_hw_demand"]
-    ) * kwh_to_mwh
-    return bers[["small_area", "heat_demand_mwh_per_y"]]
-
-
-def amalgamate_heat_demands_to_small_areas(
-    residential: pd.DataFrame, non_residential: pd.DataFrame
-) -> pd.DataFrame:
-    residential_small_areas = residential.groupby("small_area")[
-        "heat_demand_mwh_per_y"
-    ].sum()
-    index = residential_small_areas.index
-    non_residential_small_areas = (
-        non_residential.groupby("small_area")["heat_demand_mwh_per_y"]
-        .sum()
-        .reindex(index)
-        .fillna(0)
-    )
-    return (residential_small_areas + non_residential_small_areas).to_frame()
-
-
-def convert_from_mwh_per_y_to_tj_per_km2(
-    demand: pd.DataFrame, small_area_boundaries: gpd.GeoDataFrame
-) -> pd.DataFrame:
-    index = demand.index
-    m2_to_km2 = 1e-6
-    small_area_boundaries["polygon_area_km2"] = (
-        small_area_boundaries.geometry.area * m2_to_km2
-    )
-    polygon_area_km2_by_small_area = (
-        small_area_boundaries[["small_area", "polygon_area_km2"]]
-        .set_index("small_area")
-        .squeeze()
-        .reindex(index)
-    )
-    mwh_to_tj = 0.0036
-    demand_tj_per_y = (mwh_to_tj * demand).squeeze()
-    # TODO: resolve  erroneous small areas in demands upstream...
-    return (
-        (demand_tj_per_y / polygon_area_km2_by_small_area)
-        .rename("heat_demand_tj_per_km2y")
-        .to_frame()
-        .query("heat_demand_tj_per_km2y.notnull()")
+def get_parquet_result(data_dir: Path) -> results.LocalResult:
+    return results.LocalResult(
+        dir=data_dir,
+        serializer=serializers.PandasSerializer("parquet"),
     )
 
 
-def map_demand(
-    demand: pd.DataFrame, boundaries: gpd.GeoDataFrame, filepath: Path
-) -> None:
-    mapped_demands = boundaries.merge(
-        demand, left_on="small_area", right_index=True, how="left"
+def get_geoparquet_result(data_dir: Path) -> results.LocalResult:
+    return results.LocalResult(
+        dir=data_dir,
+        serializer=GeoPandasSerializer("parquet"),
     )
-    mapped_demands.to_file(filepath, driver="GeoJSON")
+
+
+load_valuation_office = prefect.task(
+    functions.read_parquet,
+    target="raw_valuation_office.parquet",
+    checkpoint=True,
+    result=get_parquet_result(DATA_DIR / "external"),
+    name="Load Dublin Valuation Office",
+)
+
+load_bers = prefect.task(
+    functions.read_parquet,
+    target="small_area_bers.parquet",
+    checkpoint=True,
+    result=get_parquet_result(DATA_DIR / "external"),
+    name="Load Dublin Small Area BERs",
+)
+
+load_benchmark_uses = prefect.task(
+    functions.read_benchmark_uses,
+    target="benchmark_uses.json",
+    checkpoint=True,
+    result=get_json_result(DATA_DIR / "external"),
+    name="Load Valuation Office Benchmark Uses",
+)
+
+load_benchmarks = prefect.task(
+    functions.read_excel,
+    target="benchmarks.parquet",
+    checkpoint=True,
+    result=get_parquet_result(DATA_DIR / "external"),
+    name="Load Energy Benchmarks",
+)
+
+load_small_area_boundaries = prefect.task(
+    functions.read_geoparquet,
+    target="small_area_boundaries.parquet",
+    checkpoint=True,
+    result=get_geoparquet_result(DATA_DIR / "external"),
+    name="Load Dublin Small Area Boundaries",
+)
+
+load_local_authority_boundaries = prefect.task(
+    functions.read_zipped_shp,
+    target="local_authority_boundaries.parquet",
+    checkpoint=True,
+    result=get_geoparquet_result(DATA_DIR / "external"),
+    name="Load Dublin Local Authority Boundaries",
+)
+
+link_small_areas_to_local_authorities = prefect.task(
+    functions.link_small_areas_to_local_authorities,
+    name="Link Each Small Areas to their Corresponding Local Authority",
+)
+
+apply_benchmarks_to_valuation_office_floor_areas = prefect.task(
+    functions.apply_benchmarks_to_valuation_office_floor_areas,
+    name="Apply Energy Benchmarks to Valuation Office Floor Areas",
+)
+
+link_valuation_office_to_small_areas = prefect.task(
+    functions.link_valuation_office_to_small_areas,
+    target="valuation_office_small_areas.parquet",
+    checkpoint=True,
+    result=get_geoparquet_result(DATA_DIR / "interim"),
+    name="Link Valuation Office to Small Area Boundaries",
+)
+
+extract_residential_heat_demand = prefect.task(
+    functions.extract_residential_heat_demand,
+    name="Extract DEAP Residential Heat Demand",
+)
+
+amalgamate_heat_demands_to_small_areas = prefect.task(
+    functions.amalgamate_heat_demands_to_small_areas,
+    target="dublin_small_area_demand_mwh_per_y.parquet",
+    checkpoint=True,
+    result=get_parquet_result("{parameters['data_dir']}/processed"),
+    name="Amalgamate Heat Demands to Small Areas",
+)
+
+convert_from_mwh_per_y_to_tj_per_km2 = prefect.task(
+    functions.convert_from_mwh_per_y_to_tj_per_km2,
+    target="dublin_small_area_demand_tj_per_km2.parquet",
+    checkpoint=True,
+    result=get_parquet_result(DATA_DIR / "processed"),
+    name="Convert Heat Demands from MWh/year to TJ/km2",
+)
+
+map_demand = prefect.task(
+    functions.map_demand,
+    name="Map Small Area Heat Demands",
+)
