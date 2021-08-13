579eaa2e3919fdef86dc11362b92207ee04acf33
Rowan Molony <rowan.molony@codema.ie>
2021-08-13 16:41:21 +0100

    Read HV network in list comprehension
    Don't need to query the network level in the pipeline, so can use
    this cleaner approach

--- infrastructure-line-lengths/functions.py
+++ infrastructure-line-lengths/functions.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List
 from typing import Optional
+from urllib.request import urlretrieve
 
 import geopandas as gpd
 import pandas as pd
@@ -10,15 +11,11 @@ def check_file_exists(filepath: Path):
     assert filepath.exists(), f"Please download & unzip {filepath} to data/external/"
 
 
-def read_network(dirpath: Path, levels: Optional[List[str]] = None) -> gpd.GeoDataFrame:
-    network = []
-    for filepath in dirpath.iterdir():
+def download_file(url: str, filepath: Path) -> None:
+    if not filepath.exists():
+        urlretrieve(url=url, filename=str(filepath))
 
-        if levels:
-            region = gpd.read_file(filepath).query(f"`Level` == {str(levels)}")
-        else:
-            region = gpd.read_file(filepath)
 
-        network.append(region)
-
+def read_network(dirpath: Path) -> gpd.GeoDataFrame:
+    network = [gpd.read_file(filepath) for filepath in dirpath.iterdir()]
     return gpd.GeoDataFrame(pd.concat(network), crs="EPSG:29903").to_crs(epsg=2157)
