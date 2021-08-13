8c235617e084c72c9e89e3572cc5502eb278f3f7
Rowan Molony <rowan.molony@codema.ie>
2021-08-13 16:52:59 +0100

    Cache HV Network as parquet
    Initial read of dgn files is slow so caching the result as a parquet
    speeds up the pipeline on re-runs

--- infrastructure-line-lengths/tasks.py
+++ infrastructure-line-lengths/tasks.py
@@ -1,8 +1,28 @@
+from pathlib import Path
+
 import geopandas as gpd
 import prefect
+from prefect.engine import results
 
 import functions
+from globals import DATA_DIR
+
+from prefect_geopandas_serializer.serializers import GeoPandasSerializer
+
+
+def get_geoparquet_result(data_dir: Path) -> results.LocalResult:
+    return results.LocalResult(
+        dir=data_dir,
+        serializer=GeoPandasSerializer("parquet"),
+    )
+
 
 check_file_exists = prefect.task(functions.check_file_exists)
+download_file = prefect.task(functions.download_file)
 read_file = prefect.task(gpd.read_file)
-read_network = prefect.task(functions.read_network)
+read_network = prefect.task(
+    functions.read_network,
+    target="hv_network.parquet",
+    result=get_geoparquet_result(DATA_DIR / "interim"),
+    checkpoint=True,
+)
