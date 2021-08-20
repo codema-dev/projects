f6bc931841c1108fc02c10eb5c412841b53fac2b
Rowan Molony <rowan.molony@codema.ie>
2021-08-20 12:55:06 +0100

    Load Small Area Boundaries in Grid Capacity Flow

--- lv-grid-capacity/tasks.py
+++ lv-grid-capacity/tasks.py
@@ -1,21 +1,37 @@
+import geopandas as gpd
 import pandas as pd
 from prefect import task
 from prefect.engine.serializers import PandasSerializer
 from prefect.engine.results import LocalResult
+from prefect_geopandas_serializer.serializers import GeoPandasSerializer
+
 
 import functions
 from globals import DATA_DIR
 
-INPUT_FILEPATHS = {"grid": "heatmap-download-version-nov-2020.parquet"}
+INPUT_FILENAMES = {
+    "grid": "heatmap-download-version-nov-2020.parquet",
+    "small_area_boundaries": "dublin_small_area_boundaries_in_routing_keys.parquet",
+}
+
 
 create_folder_structure = task(functions.create_folder_structure)
 load_esb_substation_data = task(
     pd.read_csv,
     name="Load ESB Substation Data",
     checkpoint=True,
-    target=INPUT_FILEPATHS["grid"],
+    target=INPUT_FILENAMES["grid"],
     result=LocalResult(
         dir=DATA_DIR / "external", serializer=PandasSerializer("parquet")
+    ),
+)
+load_small_area_boundaries = task(
+    gpd.read_file,
+    name="Load Small Area Boundaries",
+    checkpoint=True,
+    target=INPUT_FILENAMES["small_area_boundaries"],
+    result=LocalResult(
+        dir=DATA_DIR / "external", serializer=GeoPandasSerializer("parquet")
     ),
 )
 convert_to_geodataframe = task(
