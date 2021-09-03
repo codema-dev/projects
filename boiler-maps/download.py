e6913d8aaa276ba14e8a5d48a572f622e35f0161
Rowan Molony <rowan.molony@codema.ie>
2021-09-03 12:11:30 +0100

    Make authenticated request to s3 via ploomber
    Ref ploomber/ploomber#326

---
+++ boiler-maps/download.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import requests
+from typing import Any
+
+import fs
+from fs.tools import copy_file_data
+
+
+def fetch_s3_file(product: str, bucket: str, upstream: str) -> None:
+    filepath = Path(product)
+    filename = filepath.name
+    if not filepath.exists():
+        s3fs = fs.open_fs(bucket)
+        with s3fs.open(filename, "rb") as remote_file:
+            with open(filepath, "wb") as local_file:
+                copy_file_data(remote_file, local_file)
---
+++ boiler-maps/environmental_variables.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+from dotenv import load_dotenv
+
+
+def load_environmental_variables(product: Any):
+    load_dotenv()
