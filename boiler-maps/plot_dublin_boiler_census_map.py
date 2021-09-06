09f7381ab9a7965835f4003506878cb2f22730fc
Rowan Molony <rowan.molony@codema.ie>
2021-09-06 17:47:15 +0100

    Save census map data to csv

--- boiler-maps/plot_dublin_boiler_census_map.py
+++ boiler-maps/plot_dublin_boiler_census_map.py
@@ -13,6 +13,8 @@
 
 # %%
 import geopandas as gpd
+import pandas as pd
+
 import pandas_bokeh
 
 # %% tags=["parameters"]
@@ -25,70 +27,81 @@ dublin_small_area_boiler_statistics = gpd.read_file(
 )
 
 # %%
+boiler_columns = [
+    "No central heating",
+    "Oil",
+    "Natural gas",
+    "Electricity",
+    "Coal (incl. anthracite)",
+    "Peat (incl. turf)",
+    "Liquid petroleum gas (LPG)",
+    "Wood (incl. wood pellets)",
+    "Other",
+    "Not stated",
+    "Total",
+]
+
+# %%
+selected_fuel_types = ["Natural gas", "Oil", "Electricity", "No central heating"]
+
+# %%
+remaining_fuel_types = [c for c in boiler_columns if c not in selected_fuel_types]
+
+# %%
+use_columns = selected_fuel_types + ["small_area", "geometry"]
+
+# %%
+other = (
+    dublin_small_area_boiler_statistics[remaining_fuel_types]
+    .sum(axis=1)
+    .rename("Other")
+)
+
+# %%
+boiler_map = pd.concat(
+    [other, dublin_small_area_boiler_statistics[use_columns]], axis=1
+)
+
+# %%
 pandas_bokeh.output_file(product["map"])
 
 # %%
 hovertool_string = """
-    <h2>@edname<br>@small_area</h2>
+    <h2>@small_area</h2>
 
     <table style="background-color:#084594;color:#ffffff">
         <tr>
             <th>Boiler<br>Type</th>
-            <th>Number of Dwellings</th>
-        </tr>
-        <tr>
-            <td>No central heating</td>
-            <td>@{No Central Heating}</td>
+            <th>Number of<br>Dwellings</th>
         </tr>
         <tr>
             <td>Natural gas</td>
             <td>@{Natural gas}</td>
         </tr>
         <tr>
-            <td>Electricity</td>
-            <td>@Electricity</td>
-        </tr>
-        <tr>
             <td>Oil</td>
             <td>@Oil</td>
         </tr>
         <tr>
-            <td>Coal</td>
-            <td>@{Coal (incl. anthracite)}</td>
+            <td>Electricity</td>
+            <td>@Electricity</td>
         </tr>
         <tr>
-            <td>Peat</td>
-            <td>@{Peat (incl. turf)}</td>
-        </tr>
-        <tr>
-            <td>LPG</td>
-            <td>@{Liquid petroleum gas (LPG)}</td>
-        </tr>
-        <tr>
-            <td>Wood</td>
-            <td>@{Wood (incl. wood pellets)}</td>
+            <td>No central heating</td>
+            <td>@{No central heating}</td>
         </tr>
         <tr>
             <td>Other</td>
             <td>@Other</td>
         </tr>
-        <tr>
-            <td>Not Stated</td>
-            <td>@{Not stated}</td>
-        </tr>
-        <tr>
-            <td><strong>Total</strong></td>
-            <td><strong>@Total</strong></td>
-        </tr>
     </table>
 """
 dublin_small_area_boiler_statistics.plot_bokeh(
-    figsize=(700, 900),
-    dropdown=[
-        "Natural gas",
-        "Electricity",
-        "Oil",
-    ],
+    figsize=(500, 500),
+    dropdown=["Natural gas", "Oil", "Electricity", "No central heating", "Other"],
     hovertool_string=hovertool_string,
-    fill_alpha=0.75,
+    fill_alpha=0.9,
 )
+
+# %%
+dublin_small_area_boiler_statistics.to_csv(product["csv"])
