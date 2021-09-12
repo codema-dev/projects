41a7a90e9451594032e88b86792f89112088364d
Rowan Molony <rowan.molony@codema.ie>
2021-09-12 14:41:15 +0100

    Extract Dublin SMR Houses

--- heritage-buildings/tasks.py
+++ heritage-buildings/tasks.py
@@ -20,3 +20,16 @@ def extract_dublin_niah_houses(upstream: Any, product: Any) -> None:
     is_a_house = dublin_niah["CLASSDESC"].str.lower().str.contains("house")
     dublin_niah_houses = dublin_niah[is_a_house]
     dublin_niah_houses.to_file(str(product), driver="GPKG")
+
+
+def extract_dublin_smr_houses(upstream: Any, product: Any) -> None:
+    small_area_boundaries = gpd.read_file(
+        str(upstream["download_dublin_small_area_boundaries"])
+    )
+    smr = gpd.read_file(
+        str(upstream["download_dublin_sites_and_monuments_record_buildings"])
+    )
+    dublin_smr = gpd.sjoin(smr.to_crs(epsg=2157), small_area_boundaries, op="within")
+    is_a_house = dublin_smr["COMPOSITION"].str.lower().str.contains("house")
+    dublin_smr_houses = dublin_smr[is_a_house]
+    dublin_smr_houses.to_file(str(product), driver="GPKG")
