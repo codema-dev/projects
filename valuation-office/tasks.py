0f481c33f33b987f41881bfa5081bd4d9b74cb5d
Rowan Molony <rowan.molony@codema.ie>
2021-09-14 08:45:09 +0100

    Estimate valuation office demands via ploomber (#101)
    * Download VO files via ploomber
    
    * Convert benchmark uses to json
    
    * Download VO as csv not parquet
    
    The parquet data wasn't raw, the csv now contains the minimum
    data
    
    * Link VO to benchmarks via uses
    
    * Weather adjust benchmarks for IRL
    
    * Save VO link to Benchmarks as map
    
    Instead of saving the entire dataframe each time i want to
    add a column, its better to save a map of PropertyNo to
    Benchmark and reading this in in a function of functions
    
    * Fill unmatched VO uses with unknwon benchmark
    
    * Replace unexpectedly large floor areas with typical
    
    * Save PropertyNo of valid VO buildings
    
    - nonzero floor area
    - has an energy demand
    
    * Save unknown benchmark uses
    
    So can easily catch future additions to VO uses that invalidly
    fall outside of a benchmark category
    
    * Merge VO with benchmarks without map
    
    Previously used found and saved a map to merge the two
    datasets which was no cleaner than just merging with a
    json of common uses
    
    * Apply benchmarks to VO
    
    * Remove prefect based environment.yml
    
    * Remove prefect pipeline & config
    
    Replaced by ploomber
    
    * Remove prefect tasks
    
    * Remove prefect task definitions
    
    * Rename ploomber_tasks to tasks

--- valuation-office/tasks.py
+++ valuation-office/tasks.py
@@ -1,99 +1,192 @@
-from os import getenv
-from pathlib import Path
+from collections import defaultdict
+import json
+from typing import Any
+from zipfile import ZipFile
 
-import prefect
-from prefect.engine import results
-from prefect.engine import serializers
-
-import functions
-from globals import BASE_DIR
-from globals import DATA_DIR
-from prefect_geopandas_serializer.serializers import GeoPandasSerializer
+import pandas as pd
 
 
-def check_if_s3_keys_are_defined() -> None:
-    message = f"""
-
-        Please create a .env file
-        
-        In this directory: {BASE_DIR.resolve()}
-
-        With the following contents:
-        
-        AWS_ACCESS_KEY_ID=YOUR_KEY
-        AWS_SECRET_ACCESS_KEY=YOUR_SECRET_KEY
-    """
-    assert getenv("AWS_ACCESS_KEY_ID") is not None, message
-    assert getenv("AWS_SECRET_ACCESS_KEY") is not None, message
+def convert_benchmark_uses_to_json(upstream: Any, product: Any) -> None:
+    uses_grouped_by_category = defaultdict()
+    with ZipFile(upstream["download_benchmark_uses"]) as zf:
+        for filename in zf.namelist():
+            name = filename.split("/")[-1].replace(".txt", "")
+            with zf.open(filename, "r") as f:
+                uses_grouped_by_category[name] = [
+                    line.rstrip().decode("utf-8") for line in f
+                ]
+    benchmark_uses = {i: k for k, v in uses_grouped_by_category.items() for i in v}
+    with open(product, "w") as f:
+        json.dump(benchmark_uses, f)
 
 
-def get_json_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=serializers.JSONSerializer(),
+def weather_adjust_benchmarks(upstream: Any, product: Any) -> None:
+
+    benchmarks = pd.read_csv(upstream["download_benchmarks"])
+
+    # 5y average for Dublin Airport from 2015 to 2020
+    dublin_degree_days = 2175
+    tm46_degree_days = 2021
+    degree_day_factor = dublin_degree_days / tm46_degree_days
+
+    fossil_fuel = benchmarks["Typical fossil fuel [kWh/m²y]"] * benchmarks[
+        "% fossil fuel pro-rated to degree days"
+    ] * degree_day_factor + benchmarks["Typical fossil fuel [kWh/m²y]"] * (
+        1 - benchmarks["% fossil fuel pro-rated to degree days"]
+    )
+    fossil_fuel_heat = fossil_fuel * benchmarks["% suitable for DH or HP"]
+
+    industrial_heat = (
+        benchmarks["Industrial space heat [kWh/m²y]"] * degree_day_factor
+        + benchmarks["Industrial process energy [kWh/m²y]"]
+        * benchmarks["% suitable for DH or HP"]
     )
 
-
-def get_parquet_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=serializers.PandasSerializer("parquet"),
+    normalised_benchmarks = pd.DataFrame(
+        {
+            "Benchmark": benchmarks["Benchmark"],
+            "typical_area_m2": benchmarks["Typical Area [m²]"],
+            "area_upper_bound_m2": benchmarks["Area Upper Bound [m²]"],
+            "typical_electricity_kwh_per_m2y": benchmarks[
+                "Typical electricity [kWh/m²y]"
+            ],
+            "typical_fossil_fuel_kwh_per_m2y": fossil_fuel,
+            "typical_building_energy_kwh_per_m2y": benchmarks[
+                "Industrial building total [kWh/m²y]"
+            ],
+            "typical_process_energy_kwh_per_m2y": benchmarks[
+                "Industrial process energy [kWh/m²y]"
+            ],
+            "typical_fossil_fuel_heat_kwh_per_m2y": fossil_fuel_heat,
+            "typical_industrial_heat_kwh_per_m2y": industrial_heat,
+        }
     )
 
+    normalised_benchmarks.to_csv(product)
 
-def get_geoparquet_result(data_dir: Path) -> results.LocalResult:
-    return results.LocalResult(
-        dir=data_dir,
-        serializer=GeoPandasSerializer("parquet"),
+
+def replace_unexpectedly_large_floor_areas_with_typical_values(
+    upstream: Any, product: Any
+) -> None:
+    buildings = pd.read_csv(upstream["download_buildings"])
+    benchmarks = pd.read_csv(upstream["weather_adjust_benchmarks"], index_col=0)
+    with open(upstream["convert_benchmark_uses_to_json"], "r") as f:
+        benchmark_uses = json.load(f)
+
+    buildings["Benchmark"] = (
+        buildings["Use1"].map(benchmark_uses).rename("Benchmark").fillna("Unknown")
+    )
+    buildings_with_benchmarks = buildings.merge(benchmarks)
+
+    bounded_area_m2 = buildings_with_benchmarks["Total_SQM"].rename("bounded_area_m2")
+    typical_area = buildings_with_benchmarks["typical_area_m2"]
+
+    greater_than_zero_floor_area = buildings_with_benchmarks["Total_SQM"] > 0
+    greater_than_typical_benchmark_upper_bound = (
+        buildings_with_benchmarks["Total_SQM"]
+        > buildings_with_benchmarks["area_upper_bound_m2"]
+    )
+    valid_benchmark = ~buildings_with_benchmarks["Benchmark"].isin(["Unknown", "None"])
+    area_is_greater_than_expected = (
+        greater_than_zero_floor_area
+        & greater_than_typical_benchmark_upper_bound
+        & valid_benchmark
+    )
+    bounded_area_m2.loc[area_is_greater_than_expected] = typical_area.loc[
+        area_is_greater_than_expected
+    ]
+
+    propertyno_bounded_area_map = pd.concat(
+        [buildings["PropertyNo"], bounded_area_m2], axis=1
     )
 
+    propertyno_bounded_area_map.to_csv(product, index=False)
 
-load_valuation_office = prefect.task(
-    functions.read_parquet,
-    target="raw_valuation_office.parquet",
-    checkpoint=True,
-    result=get_parquet_result(DATA_DIR / "external"),
-    name="Load Dublin Valuation Office",
-)
 
-load_benchmark_uses = prefect.task(
-    functions.read_benchmark_uses,
-    target="benchmark_uses.json",
-    checkpoint=True,
-    result=get_json_result(DATA_DIR / "external"),
-    name="Load Valuation Office Benchmark Uses",
-)
+def save_unknown_benchmark_uses(upstream: Any, product: Any) -> None:
+    buildings = pd.read_csv(upstream["download_buildings"])
+    benchmarks = pd.read_csv(upstream["weather_adjust_benchmarks"], index_col=0)
+    with open(upstream["convert_benchmark_uses_to_json"], "r") as f:
+        benchmark_uses = json.load(f)
 
-load_benchmarks = prefect.task(
-    functions.read_csv,
-    target="benchmarks.parquet",
-    checkpoint=True,
-    result=get_parquet_result(DATA_DIR / "external"),
-    name="Load Energy Benchmarks",
-)
+    buildings["Benchmark"] = (
+        buildings["Use1"].map(benchmark_uses).rename("Benchmark").fillna("Unknown")
+    )
+    buildings_with_benchmarks = buildings.merge(benchmarks)
 
-load_small_area_boundaries = prefect.task(
-    functions.read_file,
-    target="small_area_boundaries.parquet",
-    checkpoint=True,
-    result=get_geoparquet_result(DATA_DIR / "external"),
-    name="Load Dublin Small Area Boundaries",
-)
+    benchmark_is_unknown = buildings_with_benchmarks["Benchmark"] == "Unknown"
+    unknown_benchmark_uses = pd.Series(
+        buildings_with_benchmarks.loc[benchmark_is_unknown, "Use1"].unique(),
+        name="Use1",
+    )
+    unknown_benchmark_uses.to_csv(product, index=False)
 
-link_valuation_office_to_small_areas = prefect.task(
-    functions.link_valuation_office_to_small_areas,
-    target="valuation_office_small_areas.parquet",
-    checkpoint=True,
-    result=get_geoparquet_result(DATA_DIR / "interim"),
-    name="Link Valuation Office to Small Area Boundaries",
-)
 
-link_valuation_office_to_benchmarks = prefect.task(
-    functions.link_valuation_office_to_benchmarks,
-)
-apply_benchmarks_to_valuation_office_floor_areas = prefect.task(
-    functions.apply_benchmarks_to_valuation_office_floor_areas
-)
-estimate_heat_demand = prefect.task(functions.estimate_heat_demand)
+def apply_energy_benchmarks_to_floor_areas(
+    upstream: Any, product: Any, boiler_efficiency: float
+) -> None:
 
-to_csv = prefect.task(functions.to_csv, name="Save to CSV")
+    buildings = pd.read_csv(upstream["download_buildings"])
+    benchmarks = pd.read_csv(upstream["weather_adjust_benchmarks"], index_col=0)
+    with open(upstream["convert_benchmark_uses_to_json"], "r") as f:
+        benchmark_uses = json.load(f)
+
+    buildings["Benchmark"] = (
+        buildings["Use1"].map(benchmark_uses).rename("Benchmark").fillna("Unknown")
+    )
+    buildings_with_benchmarks = buildings.merge(benchmarks)
+
+    # Replace invalid floor areas with typical values
+    bounded_area_m2 = buildings_with_benchmarks["Total_SQM"].rename("bounded_area_m2")
+    greater_than_zero_floor_area = buildings_with_benchmarks["Total_SQM"] > 0
+    greater_than_typical_benchmark_upper_bound = (
+        buildings_with_benchmarks["Total_SQM"]
+        > buildings_with_benchmarks["area_upper_bound_m2"]
+    )
+    valid_benchmark = ~buildings_with_benchmarks["Benchmark"].isin(["Unknown", "None"])
+    area_is_greater_than_expected = (
+        greater_than_zero_floor_area
+        & greater_than_typical_benchmark_upper_bound
+        & valid_benchmark
+    )
+    bounded_area_m2.loc[area_is_greater_than_expected] = buildings_with_benchmarks[
+        "typical_area_m2"
+    ].loc[area_is_greater_than_expected]
+    buildings_with_benchmarks["bounded_area_m2"] = bounded_area_m2
+
+    # Apply Benchmarks
+    kwh_to_mwh = 1e-3
+    buildings_with_benchmarks["electricity_demand_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_electricity_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+    ).fillna(0)
+    buildings_with_benchmarks["fossil_fuel_demand_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_fossil_fuel_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+        * boiler_efficiency
+    ).fillna(0)
+    buildings_with_benchmarks["building_energy_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_building_energy_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+    )
+    buildings_with_benchmarks["process_energy_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_process_energy_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+    )
+    buildings_with_benchmarks["fossil_fuel_heat_demand_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_fossil_fuel_heat_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+        * boiler_efficiency
+    ).fillna(0)
+    buildings_with_benchmarks["industrial_heat_demand_mwh_per_y"] = (
+        bounded_area_m2.fillna(0)
+        * buildings_with_benchmarks["typical_industrial_heat_kwh_per_m2y"].fillna(0)
+        * kwh_to_mwh
+    )
+
+    buildings_with_benchmarks.to_csv(product)
