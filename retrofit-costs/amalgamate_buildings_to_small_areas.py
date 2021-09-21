2a85182b098e281aad8341243bc75952e0d56d14
Rowan Molony <rowan.molony@codema.ie>
2021-09-21 10:26:43 +0100

    Estimate total retrofit costs in notebook

--- retrofit-costs/amalgamate_buildings_to_small_areas.py
+++ retrofit-costs/amalgamate_buildings_to_small_areas.py
@@ -67,3 +67,23 @@ small_area_boundaries.merge(percentage_viable_for_heat_pumps).plot(
 )
 
 percentage_viable_for_heat_pumps.to_csv(product["heat_pump_viability"])
+
+## Estimate Total Retrofits by Measure
+
+is_retrofitted_columns = [c for c in retrofit_costs.columns if "_is_retrofitted" in c]
+
+retrofit_costs[is_retrofitted_columns].sum()
+
+retrofit_costs[is_retrofitted_columns].any(axis=1).sum()
+
+## Estimate All of Dublin Costs
+
+cost_columns = [c for c in retrofit_costs.columns if "cost" in c]
+retrofit_costs[cost_columns].sum().divide(1e6)
+
+lower_cost_columns = [c for c in retrofit_costs.columns if "cost_lower" in c]
+upper_cost_columns = [c for c in retrofit_costs.columns if "cost_upper" in c]
+
+retrofit_costs[lower_cost_columns].sum().divide(1e6).sum()
+
+retrofit_costs[upper_cost_columns].sum().divide(1e6).sum()
