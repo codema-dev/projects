e5f8ba14a8529f04667c85b760444181c4dfe3ff
Rowan Molony <rowan.molony@codema.ie>
2021-09-23 17:52:27 +0100

    Amalgamate Syn BER Gas Demand to Postcode level
    For comparison vs CSO Gas

--- compare-synthetic-bers-to-cso-gas/tasks.py
+++ compare-synthetic-bers-to-cso-gas/tasks.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import numpy as np
 import pandas as pd
 
 
@@ -17,3 +18,31 @@ def concatenate_dublin_postal_districts_and_county_dublin(
     )
     dublin = pd.concat([county.loc[["Dublin County"], :], postal_district])
     dublin.to_csv(product)
+
+
+def amalgamate_synthetic_bers_to_postcode_heat(upstream: Any, product: Any) -> None:
+    bers = pd.read_parquet(upstream["download_synthetic_bers"])
+    gas_bers = bers.query("main_sh_boiler_fuel == 'Mains Gas'")
+    gas_consumption = (
+        gas_bers["main_sh_demand"]
+        + np.where(
+            gas_bers["main_hw_boiler_fuel"] == "Mains Gas",
+            gas_bers["main_hw_demand"],
+            0,
+        )
+        + np.where(
+            gas_bers["suppl_sh_boiler_fuel"] == "Mains Gas",
+            gas_bers["suppl_sh_demand"],
+            0,
+        )
+    )
+    postcode_gas_consumption = (
+        pd.concat([gas_bers["countyname"], gas_consumption], axis=1)
+        .groupby("countyname")
+        .sum()
+        .squeeze()
+        .divide(1e6)
+        .round()
+        .rename("synthetic_ber_heat_demand")
+    )
+    postcode_gas_consumption.to_csv(product)
