a3bf64d9218179420f2c397086c701c92a7b6ddc
Rowan Molony <rowan.molony@codema.ie>
2021-09-28 10:14:40 +0100

    Extract HV Line Lengths

--- electricity-line-lengths/tasks.py
+++ electricity-line-lengths/tasks.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Any
+from typing import List
 
 import geopandas as gpd
 import pandas as pd
@@ -25,3 +26,18 @@ def convert_mv_lv_network_to_parquet(
     network = [gpd.read_file(Path(dirpath) / f"{id}.dgn") for id in dublin_mv_index]
     mv_lv_network = gpd.GeoDataFrame(pd.concat(network), crs="EPSG:29903")
     mv_lv_network.to_crs(epsg=2157).to_parquet(product)
+
+
+def extract_hv_line_lengths(
+    product: Any,
+    upstream: Any,
+    levels: List[str],
+    columns: List[str],
+) -> gpd.GeoDataFrame:
+    hv_network = gpd.read_parquet(upstream["convert_hv_data_to_parquet"])
+    level_is_a_line = hv_network["Level"].isin(levels)
+    lines = hv_network[level_is_a_line]
+    line_lengths = pd.concat(
+        [lines[columns], lines.geometry.length.rename("line_length_m")], axis=1
+    )
+    line_lengths.to_parquet(product)
