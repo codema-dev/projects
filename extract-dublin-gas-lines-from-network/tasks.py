5ccbaa7ab7c1802b43f9727d4fbc30207303f561
Rowan Molony <rowan.molony@codema.ie>
2021-09-28 16:50:22 +0100

    Extract lines in SAs

--- extract-dublin-gas-lines-from-network/tasks.py
+++ extract-dublin-gas-lines-from-network/tasks.py
@@ -12,7 +12,25 @@ def _check_gni_data_is_uploaded(filepath: str) -> None:
     assert Path(filepath).exists(), message
 
 
-def save_gni_data_to_gpkg(product: Any, filepaths: List[str]) -> None:
+def save_gni_data_to_parquet(product: Any, filepaths: List[str]) -> None:
     _check_gni_data_is_uploaded(filepaths[0])
     lines = pd.concat([gpd.read_file(f, crs="EPSG:2157") for f in filepaths])
-    lines.to_file(str(product), driver="GPKG")
+    lines.to_parquet(product)
+
+
+def extract_lines_in_small_area_boundaries(
+    product: Any,
+    upstream: Any,
+) -> None:
+    lines = gpd.read_parquet(upstream["save_gni_data_to_parquet"])
+    dublin_small_area_boundaries = gpd.read_file(
+        str(upstream["download_dublin_small_area_boundaries"])
+    )
+
+    lines_in_boundaries = gpd.overlay(
+        lines,
+        dublin_small_area_boundaries[["small_area", "geometry"]],
+        "intersection",
+    )
+
+    lines_in_boundaries.to_file(str(product), driver="GPKG")
