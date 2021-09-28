7c09d5987b8f5bef041dce08a9bc5dd076204d1a
Rowan Molony <rowan.molony@codema.ie>
2021-09-28 22:04:28 +0100

    Plot DH LA feasibility tables

--- plot-district-heating-viability/plot_heat_demand_density_tables.py
+++ plot-district-heating-viability/plot_heat_demand_density_tables.py
@@ -5,50 +5,46 @@ import numpy as np
 import pandas as pd
 import dataframe_image as dfi
 
-import pandas_bokeh
-from globals import DATA_DIR
-
 pd.set_option("display.precision", 1)
 
 ## Parametrize
 # overwrite parameters with arguemnts generated in prefect pipeline
 
 # + tags=["parameters"]
-SAVE_AS_HTML: bool = True
-SAVE_AS_IMAGE: bool = True
-DATA_DIR: Path = Path(DATA_DIR)
-hdd_map_filepath: Path = (
-    DATA_DIR / "processed" / "dublin_small_area_demand_tj_per_km2.gpkg"
-)
+upstream = ["estimate_heat_demand_density", "download_dublin_small_area_boundaries"]
+product = None
 # -
 
-if not SAVE_AS_HTML:
-    pandas_bokeh.output_notebook()
+Path(product["table_dir"]).mkdir(exist_ok=True)
 
 ## Load
 
-hdd_map = gpd.read_file(hdd_map_filepath)
+demand = pd.read_csv(upstream["estimate_heat_demand_density"]["demand"])
+
+density = pd.read_csv(upstream["estimate_heat_demand_density"]["density"])
+
+boundaries = gpd.read_file(upstream["download_dublin_small_area_boundaries"])
 
 ## Set Globals
 
-local_authorities = hdd_map["local_authority"].unique()
+local_authorities = boundaries["local_authority"].unique()
+
+## Create Map
+
+density_map = boundaries.merge(density).merge(demand)
 
 ## Calculate Totals
 
-hdd_map["total_heat_demand_tj_per_km2y"] = (
-    hdd_map["residential_heat_demand_tj_per_km2y"]
-    + hdd_map["non_residential_heat_demand_tj_per_km2y"]
-)
+columns = [c for c in density_map.columns if "tj_per_km2" in c]
+density_map["total_heat_tj_per_km2"] = density_map[columns].sum(axis=1)
 
-hdd_map["total_heat_demand_mwh_per_y"] = (
-    hdd_map["residential_heat_demand_mwh_per_y"]
-    + hdd_map["non_residential_heat_demand_mwh_per_y"]
-)
+columns = [c for c in density_map.columns if "mwh" in c]
+density_map["total_heat_mwh"] = density_map[columns].sum(axis=1)
 
 ## Categorise demands
 
-hdd_map["feasibility"] = pd.cut(
-    hdd_map["total_heat_demand_tj_per_km2y"],
+density_map["feasibility"] = pd.cut(
+    density_map["total_heat_tj_per_km2"],
     bins=[-np.inf, 20, 50, 120, 300, np.inf],
     labels=[
         "Not Feasible",
@@ -58,25 +54,28 @@ hdd_map["feasibility"] = pd.cut(
         "Very Feasible",
     ],
 )
-hdd_map["category"] = hdd_map["feasibility"].cat.codes
+
+density_map["category"] = density_map["feasibility"].cat.codes
 
 ## Amalgamate Demands to Local Authority Level
 
 use_columns = [
     "local_authority",
-    "residential_heat_demand_mwh_per_y",
-    "non_residential_heat_demand_mwh_per_y",
-    "total_heat_demand_mwh_per_y",
+    "residential_heat_mwh",
+    "commercial_heat_mwh",
+    "industrial_heat_mwh",
+    "total_heat_mwh",
     "feasibility",
 ]
-hdd_map_table = (
-    hdd_map.loc[:, use_columns]
+density_map_table = (
+    density_map.loc[:, use_columns]
     .groupby(["local_authority", "feasibility"])
     .sum()
     .round()
     .reset_index()
 )
-hdd_map_table["band"] = hdd_map_table["feasibility"].map(
+
+density_map_table["band"] = density_map_table["feasibility"].map(
     {
         "Not Feasible": "<20",
         "Future Potential": "20-50",
@@ -91,17 +90,16 @@ hdd_map_table["band"] = hdd_map_table["feasibility"].map(
 for la in local_authorities:
 
     la_table = (
-        hdd_map_table.query("local_authority == @la")
+        density_map_table.query("local_authority == @la")
         .copy()
         .reset_index(drop=True)
         .drop(columns="local_authority")
     )
-    total_heat = la_table["total_heat_demand_mwh_per_y"].sum()
+
+    total_heat = la_table["total_heat_mwh"].sum()
+
     la_table["percentage_share_of_heat_demand"] = (
-        la_table["total_heat_demand_mwh_per_y"]
-        .divide(total_heat)
-        .multiply(100)
-        .round(1)
+        la_table["total_heat_mwh"].divide(total_heat).multiply(100).round(1)
     )
 
     ## Style each table row with it's corresponding color
@@ -109,17 +107,19 @@ for la in local_authorities:
     styled_table = (
         la_table.astype(
             {
-                "residential_heat_demand_mwh_per_y": "int32",
-                "non_residential_heat_demand_mwh_per_y": "int32",
-                "total_heat_demand_mwh_per_y": "int32",
+                "residential_heat_mwh": "int32",
+                "commercial_heat_mwh": "int32",
+                "industrial_heat_mwh": "int32",
+                "total_heat_mwh": "int32",
             }
         )
         .rename(
             columns={
                 "feasibility": "Feasibility",
-                "residential_heat_demand_mwh_per_y": "Residential [MWh/year]",
-                "non_residential_heat_demand_mwh_per_y": "Non-Residential [MWh/year]",
-                "total_heat_demand_mwh_per_y": "Total [MWh/year]",
+                "residential_heat_mwh": "Residential [MWh/year]",
+                "commercial_heat_mwh": "Commercial [MWh/year]",
+                "industrial_heat_mwh": "Industrial [MWh/year]",
+                "total_heat_mwh": "Total [MWh/year]",
                 "band": "Band [TJ/km²year]",
                 "percentage_share_of_heat_demand": "% Share [MWh/year]",
             }
@@ -127,6 +127,7 @@ for la in local_authorities:
         .set_index("Feasibility")
         .style
     )
+
     feasibilities = [
         "Not Feasible",
         "Future Potential",
@@ -150,13 +151,9 @@ for la in local_authorities:
         )
 
     filename = la.replace(" ", "-").replace("ú", "u")
-    if SAVE_AS_HTML:
-        with open(
-            Path(DATA_DIR) / "maps" / f"Glossary-{filename}.html", "w", encoding="utf-8"
-        ) as file:
-            styled_table.to_html(file)
-
-    if SAVE_AS_IMAGE:
-        dfi.export(
-            styled_table, str(Path(DATA_DIR) / "maps" / f"Glossary-{filename}.png")
-        )
+    with open(
+        Path(product["table_dir"]) / f"Glossary-{filename}.html",
+        "w",
+        encoding="utf-8",
+    ) as file:
+        styled_table.to_html(file)
