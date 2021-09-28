55828104f1728971971d58f00219081c564cce06
Rowan Molony <rowan.molony@codema.ie>
2021-09-28 09:53:25 +0100

    Convert Dublin MV-LV data to parquet

--- electricity-line-lengths/tasks.py
+++ electricity-line-lengths/tasks.py
@@ -1,4 +1,4 @@
-from pathlb import Path
+from pathlib import Path
 from typing import Any
 
 import geopandas as gpd
@@ -15,3 +15,13 @@ def convert_hv_data_to_parquet(product: Any, dirpath: str) -> gpd.GeoDataFrame:
     network = [gpd.read_file(filepath) for filepath in Path(dirpath).iterdir()]
     hv_network = gpd.GeoDataFrame(pd.concat(network), crs="EPSG:29903")
     hv_network.to_crs(epsg=2157).to_parquet(product)
+
+
+def convert_mv_lv_network_to_parquet(
+    product: Any, upstream: Any, dirpath: str
+) -> gpd.GeoDataFrame:
+    _check_esb_data_is_uploaded(dirpath)
+    dublin_mv_index = pd.read_csv(upstream["download_dublin_mv_index"], squeeze=True)
+    network = [gpd.read_file(Path(dirpath) / f"{id}.dgn") for id in dublin_mv_index]
+    mv_lv_network = gpd.GeoDataFrame(pd.concat(network), crs="EPSG:29903")
+    mv_lv_network.to_crs(epsg=2157).to_parquet(product)
