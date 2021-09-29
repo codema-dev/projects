2f95227892b401ac862f7d0d44cbed805364cde0
Rowan Molony <rowan.molony@codema.ie>
2021-09-29 12:13:32 +0100

    Split heat pump viability map to its own nb

--- plot-heat-pump-viability/estimate_stock_statistics.py
+++ plot-heat-pump-viability/estimate_stock_statistics.py
@@ -8,19 +8,15 @@ sns.set()
 # + tags=["parameters"]
 upstream = [
     "download_buildings",
-    "download_small_area_boundaries",
     "estimate_retrofit_costs",
     "estimate_retrofit_energy_saving",
     "estimate_retrofit_energy_saving_with_rebound",
-    "estimate_retrofit_hlp_improvement",
     "estimate_retrofit_ber_rating_improvement",
 ]
 product = None
 # -
 
 ## Load
-
-small_area_boundaries = gpd.read_file(upstream["download_small_area_boundaries"])
 
 pre_retrofit = pd.read_parquet(upstream["download_buildings"])
 
@@ -32,42 +28,11 @@ energy_saving_with_rebound = pd.read_csv(
     upstream["estimate_retrofit_energy_saving_with_rebound"]
 )
 
-hlp_improvement = pd.read_csv(upstream["estimate_retrofit_hlp_improvement"])
-
 ber_improvement = pd.read_csv(upstream["estimate_retrofit_ber_rating_improvement"])
 
 ## Plot Post-Retrofit BERs
 
 ber_improvement["energy_rating"].value_counts().sort_index().plot.bar()
-
-
-## Map Heat Pump Viability
-
-hlp_improvement["is_viable_for_a_heat_pump"] = (
-    hlp_improvement["post_retrofit_heat_loss_parameter"] < 2
-)
-
-hlp_improvement["is_viable_for_a_heat_pump"].sum() / len(hlp_improvement)
-
-small_areas_viable_for_heat_pumps = hlp_improvement.groupby("small_area")[
-    "is_viable_for_a_heat_pump"
-].sum()
-
-small_area_total_buildings = hlp_improvement.groupby("small_area").size()
-
-percentage_viable_for_heat_pumps = (
-    small_areas_viable_for_heat_pumps.divide(small_area_total_buildings)
-    .multiply(100)
-    .rename("percentage_viable_for_heat_pumps")
-    .reset_index()
-)
-
-small_area_boundaries.merge(percentage_viable_for_heat_pumps).plot(
-    column="percentage_viable_for_heat_pumps",
-    figsize=(20, 20),
-    legend=True,
-    linewidth=0.5,
-)
 
 ## Estimate Total Retrofits by Measure
 
@@ -159,9 +124,3 @@ pre_retrofit_sh_vs_hw_with_rebound.plot.pie(
     autopct="%1.1f%%",
     title="With Rebound Effect Considered",
 )
-
-## Save Data
-
-small_area_total_buildings.to_csv(product["total_buildings"])
-
-percentage_viable_for_heat_pumps.to_csv(product["heat_pump_viability"])
