2eab6af8a64e00350f760d0d9994c8b07bf40fc3
Rowan Molony <rowan.molony@codema.ie>
2021-09-29 13:09:39 +0100

    Split plot retrofit costs to own nb

--- plot-heat-pump-viability/estimate_stock_statistics.py
+++ plot-heat-pump-viability/estimate_stock_statistics.py
@@ -8,7 +8,6 @@ sns.set()
 # + tags=["parameters"]
 upstream = [
     "download_buildings",
-    "estimate_retrofit_costs",
     "estimate_retrofit_energy_saving",
     "estimate_retrofit_energy_saving_with_rebound",
 ]
@@ -19,33 +18,11 @@ product = None
 
 pre_retrofit = pd.read_parquet(upstream["download_buildings"])
 
-retrofit_costs = pd.read_csv(upstream["estimate_retrofit_costs"])
-
 energy_saving = pd.read_csv(upstream["estimate_retrofit_energy_saving"])
 
 energy_saving_with_rebound = pd.read_csv(
     upstream["estimate_retrofit_energy_saving_with_rebound"]
 )
-
-## Estimate Total Retrofits by Measure
-
-is_retrofitted_columns = [c for c in retrofit_costs.columns if "_is_retrofitted" in c]
-
-retrofit_costs[is_retrofitted_columns].sum()
-
-retrofit_costs[is_retrofitted_columns].any(axis=1).sum()
-
-## Estimate All of Dublin Costs
-
-cost_columns = [c for c in retrofit_costs.columns if "cost" in c]
-retrofit_costs[cost_columns].sum().divide(1e6)
-
-lower_cost_columns = [c for c in retrofit_costs.columns if "cost_lower" in c]
-upper_cost_columns = [c for c in retrofit_costs.columns if "cost_upper" in c]
-
-retrofit_costs[lower_cost_columns].sum().divide(1e6).sum()
-
-retrofit_costs[upper_cost_columns].sum().divide(1e6).sum()
 
 ## Estimate Energy & Emission Savings
 
