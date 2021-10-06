69194ace1445b01b75d5a96904bee1eaea86db07
Rowan Molony <rowan.molony@codema.ie>
2021-10-06 16:22:39 +0100

    Melt small area period built to individual buildings

--- extract-ireland-small-area-building-ages-from-census/tasks.py
+++ extract-ireland-small-area-building-ages-from-census/tasks.py
@@ -15,3 +15,35 @@ def extract_period_built_statistics(product: Any, upstream: Any) -> None:
     select_statistics.to_csv(product, index=False)
 
 
+def _repeat_rows_on_column(df, on):
+    return df.reindex(df.index.repeat(df[on])).drop(columns=on)
+
+
+def melt_small_area_period_built_to_individual_buildings(
+    product: Any, upstream: Any,
+) -> None:
+    """Wrangle the stock to individual building level.
+
+    Before:
+        GEOGID              T6_2_PRE19H     ...
+        SA2017_017001001    19              ...
+
+    After:
+        small_area          period_built
+        017001001           PRE19H
+        017001001           PRE19H
+
+    Args:
+        sa_stats_raw (pd.DataFrame): overview of buildings
+
+    Returns:
+        pd.DataFrame: individual buildings
+    """
+    small_area_period_built = pd.read_csv(upstream["extract_period_built_statistics"])
+    individual_building_period_built =  (
+        small_area_period_built.melt(id_vars="small_area", var_name="period_built", value_name="total")
+        .query("period_built != 'T'")
+        .pipe(_repeat_rows_on_column, on="total")
+    )
+    individual_building_period_built.to_csv(product, index=False)
+
