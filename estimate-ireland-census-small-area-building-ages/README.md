7382c93546d4da4ee36284f94437f65d866c201a
Rowan Molony <rowan.molony@codema.ie>
2021-10-06 16:46:59 +0100

    Estimate Ireland small area building ages (#116)
    * Extract Period Built Statistics
    
    * Melt small area period built to individual buildings
    
    * Infer census building ages on modal values
    
    * Rename to estimate census building ages
    
    As an important step is guessing building ages based on the modal
    value for that area
    
    * Add README

---
+++ estimate-ireland-census-small-area-building-ages/README.md
@@ -0,0 +1,33 @@
+---
+jupytext:
+  cell_metadata_filter: -all
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.12.0
+kernelspec:
+  display_name: Python 3 (ipykernel)
+  language: python
+  name: python3
+---
+
+# Estimate Census Small Area Building Ages
+
+## What `pipeline.yaml` is doing:
+
+![pipeline.png](pipeline.png)
+
+## Run pipeline
+
+On Binder:
+
+```{code-cell} ipython3
+!ploomber build
+```
+
+OR on your Terminal:
+
+```{code-cell} ipython3
+ploomber build
+```
---
+++ estimate-ireland-census-small-area-building-ages/tasks.py
@@ -0,0 +1,63 @@
+import re
+from typing import Any
+
+import numpy as np
+import pandas as pd
+
+
+def extract_period_built_statistics(product: Any, upstream: Any) -> None:
+    statistics = pd.read_csv(upstream["download_census_small_area_statistics_2016"])
+    building_age_columns = [x for x in statistics.columns if re.match(r"T6_2_.*H", x)]
+    building_age_statistics = statistics[building_age_columns].rename(
+        columns=lambda x: re.findall(f"T6_2_(.*)H", x)[0]
+    )
+    small_areas = statistics["GEOGID"].str[7:].rename("small_area")
+    select_statistics = pd.concat([small_areas, building_age_statistics], axis=1)
+    select_statistics.to_csv(product, index=False)
+
+
+def _repeat_rows_on_column(df, on):
+    return df.reindex(df.index.repeat(df[on])).drop(columns=on)
+
+
+def melt_small_area_period_built_to_individual_buildings(
+    product: Any, upstream: Any,
+) -> None:
+    """Wrangle the stock to individual building level.
+
+    Before:
+        GEOGID              T6_2_PRE19H     ...
+        SA2017_017001001    19              ...
+
+    After:
+        small_area          period_built
+        017001001           PRE19H
+        017001001           PRE19H
+
+    Args:
+        sa_stats_raw (pd.DataFrame): overview of buildings
+
+    Returns:
+        pd.DataFrame: individual buildings
+    """
+    small_area_period_built = pd.read_csv(upstream["extract_period_built_statistics"])
+    individual_building_period_built =  (
+        small_area_period_built.melt(id_vars="small_area", var_name="period_built", value_name="total")
+        .query("period_built != 'T'")
+        .pipe(_repeat_rows_on_column, on="total")
+    )
+    individual_building_period_built.to_csv(product, index=False)
+
+
+
+def replace_not_stated_period_built_with_mode(
+    product: Any, upstream: Any,
+) -> None:
+    buildings = pd.read_csv(upstream["melt_small_area_period_built_to_individual_buildings"])
+    modal_period_built = (
+        buildings.assign(period_built=lambda df: df["period_built"].replace({"NS": np.nan}))
+        .groupby("small_area")["period_built"]
+        .transform(lambda s: s.mode()[0])
+    )
+    buildings["period_built"] = buildings["period_built"].replace({"NS": np.nan}).fillna(modal_period_built)
+    buildings.to_csv(product, index=False)
